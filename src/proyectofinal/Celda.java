785795223a66943f529e625ecdc19a7dd2059182
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-05-25 21:00:57 -0600

    Creación del Repositorio para el Proyecto Final de Progra III

--- src/proyectofinal/Celda.java
+++ src/proyectofinal/Celda.java
@@ -1,18 +1,1 @@
 package proyectofinal;
-
-
-public class Celda {
-    private String valor;
-    private String formula;
-    
-    public Celda() {
-        this.valor = "";
-        this.formula = "";
-    }
-    
-    public String getValor() { return valor; }
-    public void setValor(String valor) { this.valor = valor; }
-    
-    public String getFormula() { return formula; }
-    public void setFormula(String formula) { this.formula = formula; }
-}
--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -1,16 +1,10 @@
 package proyectofinal;
 
 
-import javax.swing.SwingUtilities;
+
 
 public class ProyectoFinal {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Modelo modelo = new Modelo();
-            Vista vista = new Vista(modelo, null);
-            Controlador controlador = new Controlador(modelo, vista);
-            vista.setControlador(controlador);
-            vista.setVisible(true);
-        });
+        
     }
 }
