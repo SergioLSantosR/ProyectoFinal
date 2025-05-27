364ee585f8bef37ff4999236c446fd76043fb40b
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-05-26 22:47:42 -0600

    Actualización de clase MiListaEnlazada

--- src/proyectofinal/MiListaEnlazada.java
+++ src/proyectofinal/MiListaEnlazada.java
@@ -1,1 +1,63 @@
 package proyectofinal;
+
+
+public class MiListaEnlazada {
+    private Nodo cabeza;
+    private int tamaño;
+    
+    private class Nodo {
+        Object dato;
+        Nodo siguiente;
+        
+        Nodo(Object dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+    
+    public MiListaEnlazada() {
+        cabeza = null;
+        tamaño = 0;
+    }
+    
+    public void agregar(Object dato) {
+        Nodo nuevoNodo = new Nodo(dato);
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+        } else {
+            Nodo actual = cabeza;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevoNodo;
+        }
+        tamaño++;
+    }
+    
+    public Object obtener(int indice) {
+        if (indice < 0 || indice >= tamaño) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango");
+        }
+        
+        Nodo actual = cabeza;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.siguiente;
+        }
+        return actual.dato;
+    }
+    
+    public int tamaño() {
+        return tamaño;
+    }
+    
+    public boolean contiene(Object dato) {
+        Nodo actual = cabeza;
+        while (actual != null) {
+            if (actual.dato.equals(dato)) {
+                return true;
+            }
+            actual = actual.siguiente;
+        }
+        return false;
+    }
+}
