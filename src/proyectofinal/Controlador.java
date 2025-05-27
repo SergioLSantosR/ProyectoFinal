42af952e953dc0e7a0110a7ba87268b4138e0c67
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-05-26 22:36:34 -0600

    Depuración y actualización de clases

--- src/proyectofinal/Controlador.java
+++ src/proyectofinal/Controlador.java
@@ -1,1 +1,36 @@
 package proyectofinal;
+
+public class Controlador {
+    private Modelo modelo;
+    private Vista vista;
+    
+    public Controlador(Modelo modelo, Vista vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+    
+    public void procesarFormula(int fila, int columna, String formula) {
+    try {
+        if (formula.startsWith("=")) {
+            double resultado = modelo.getHojaActual().evaluarFormula(formula);
+            modelo.getHojaActual().getCeldas()[fila][columna].setValor(String.valueOf(resultado));
+            modelo.getHojaActual().getCeldas()[fila][columna].setFormula(formula);
+        } else {
+            modelo.getHojaActual().getCeldas()[fila][columna].setValor(formula);
+            modelo.getHojaActual().getCeldas()[fila][columna].setFormula("");
+        }
+        vista.actualizarTabla();
+    } catch (Exception e) {
+        vista.mostrarError("Error en la fórmula: " + e.getMessage());
+    }
+}
+    
+    public void crearNuevaHoja(String nombre) {
+        modelo.crearNuevaHoja(nombre);
+        vista.actualizarVista();
+    }
+    
+    public void mostrarTablaHash() {
+        vista.mostrarDialogoTablaHash();
+    }
+}
--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -1,10 +1,15 @@
 package proyectofinal;
 
-
-
+import javax.swing.SwingUtilities;
 
 public class ProyectoFinal {
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> {
+            Modelo modelo = new Modelo();
+            Vista vista = new Vista(modelo, null);
+            Controlador controlador = new Controlador(modelo, vista);
+            vista.setControlador(controlador);
+            vista.setVisible(true);
+        });
     }
 }
